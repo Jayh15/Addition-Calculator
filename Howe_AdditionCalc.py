83f4c6183b56103640fdd3b14d550d71f4262a6e
Jaylen Howe <168600994+Jayh15@users.noreply.github.com>
2025-03-06 20:55:51 -0500

    Add files via upload

---
+++ Howe_AdditionCalc.py
@@ -0,0 +1,8 @@
+def addition_calculator(num1, num2):
+    total = num1 + num2
+    print(f"The sum of {num1} and {num2} is {total}.")
+
+# Calling the function three times with different sets of numbers
+addition_calculator(5, 7)
+addition_calculator(10, 20)
+addition_calculator(15, 25)
